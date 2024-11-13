bd26976466d41cd19672a084887ba17294eb56c5
dudaborges <duda.pborges92@gmail.com>
2024-11-12 21:31:07 -0300

    :sparkles: feat: database models

--- telephony_api/src/database/models.py
+++ telephony_api/src/database/models.py
@@ -3,16 +3,16 @@ from tortoise import fields, models
 
 class CallRecord(models.Model):
     id = fields.UUIDField(pk=True)
-    type = models.CharField(max_length=10, choices=[('start', 'Start'), ('end', 'End')], null=False)
+    type = fields.CharField(max_length=10)
     timestamp = models.DateTimeField(null=False)
-    call_id = models.CharField(max_length=255, null=False, unique=True)
+    call_id = fields.IntField()
     source = models.CharField(max_length=15, null=True)
     destination = models.CharField(max_length=15, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class PhoneBill(models.Model):
     id = fields.UUIDField(pk=True)
     phone_number = fields.CharField(max_length=15) 
-    period = fields.CharField(max_length=7) 
-    total_amount = fields.DecimalField(max_digits=10, decimal_places=2) 
-    call_records = fields.ReverseRelation["CallRecord"] 
+    period = fields.DateField()
+    total_cost = fields.DecimalField(max_digits=10, decimal_places=2) 
+    records = fields.ManyToManyField('models.CallRecord', related_name='phone_bill')
