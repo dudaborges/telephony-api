03cce334e9790f3c40516c55bb0a7e33b94fc5ae
dudaborges <duda.pborges92@gmail.com>
2024-11-12 21:40:07 -0300

    :sparkles: feat: set default tortoise orm connection

--- telephony_api/src/database/models.py
+++ telephony_api/src/database/models.py
@@ -12,7 +12,9 @@ class CallRecord(models.Model):
 
 class PhoneBill(models.Model):
     id = fields.UUIDField(pk=True)
-    phone_number = fields.CharField(max_length=15) 
+    phone_number = fields.CharField(max_length=15)
     period = fields.DateField()
-    total_cost = fields.DecimalField(max_digits=10, decimal_places=2) 
-    records = fields.ManyToManyField('models.CallRecord', related_name='phone_bill')
+    total_cost = fields.DecimalField(max_digits=10, decimal_places=2)
+    records = fields.ManyToManyField(
+        'models.CallRecord', related_name='phone_bill'
+    )
